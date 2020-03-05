2a9f921d4e828dcedfc8b043c62775fcd9c947b2
戴威 <dw012345@163.com>
2020-03-06 00:54:47 +0800

    添加了联系卖家和发布商品的入口

--- app/src/main/java/com/example/graduationproject/home/PostActivity.java
+++ app/src/main/java/com/example/graduationproject/home/PostActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.example.graduationproject.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -12,5 +13,8 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
